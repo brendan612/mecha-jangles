278ba409d86bcf264f4c7e6260858f01383e0531
Brendan <brendan612@gmail.com>
2022-01-27 18:25:56 -0800

    added awaits to a couple commands

--- slash/shutup.js
+++ slash/shutup.js
@@ -12,7 +12,7 @@ module.exports = {
         let user = interaction.options.getMentionable('user');
 
         if (user.voice){
-            interaction.client.VCM.PlaySound(user.voice.channel.id, interaction.guild.id, interaction.guild.voiceAdapterCreator, './assets/sounds/shutup.mp3 ', 0, false, async function(){
+            await interaction.client.VCM.PlaySound(user.voice.channel.id, interaction.guild.id, interaction.guild.voiceAdapterCreator, './assets/sounds/shutup.mp3 ', 0, false, async function(){
                 interaction.guild.members.fetch(user).then(member => {
                     member.voice.setChannel(null);
                 });
